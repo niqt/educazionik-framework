fc8c13c64e861e4b4ad815838c976dbf159129e2
Nicola De Filippo <nicola@nicoladefilippo.it>
2013-11-08 00:41:07 +0100

    Initi parser xml

--- set.cpp
+++ set.cpp
@@ -20,13 +20,27 @@ void Set::load(QString fileName)
 
         xmlStream.readNext();
         //if(xmlStream.isStartElement())
-        qDebug() << xmlStream.text();
-       qDebug() << xmlStream.tokenString() << "\n";
-        if (xmlStream.tokenType() == QXmlStreamReader::StartDocument)
+        //qDebug() << xmlStream.text();
+       //qDebug() << xmlStream.tokenString() << "\n";
+        if (xmlStream.tokenType() == QXmlStreamReader::StartElement)
         {
             // Read the tag name.
-            QString sec(xmlStream.tokenString());
-            qDebug() << " sec " << sec << endl;
+
+            if (xmlStream.name() == "exercise") {
+                 qDebug() << xmlStream.name();
+                xmlStream.readNext();
+
+                qDebug() << "NAME " << xmlStream.readElementText();
+                if (xmlStream.readElementText() == "setA") {
+                    qDebug() << xmlStream.name();
+                    xmlStream.readNext();
+                    while (xmlStream.readElementText() == "element") {
+                        if (xmlStream.name() == "element")
+                            qDebug() << "card " << xmlStream.attributes().value("cardinality").toString() << endl;
+                        xmlStream.readNext();
+                    }
+                }
+            }
             // Check in settings map, whether there's already an entry. If not, insert.
             //if(!_settingsMap.contains(sec))
             //    _settingsMap.insert(sec, xmlStream.attributes());
